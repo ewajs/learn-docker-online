e6d5adb3123e3cc16a6fcd591864510a94504112
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2019-08-24 17:21:29 -0300

    Added excercise5.sh to spin up everything

---
+++ Excercise5/excercise5.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+docker network create mynet
+
+docker container run -d --network mynet --name db --env-file db.env postgres
+
+docker container run -d --network mynet --network-alias webapp --name app1 --env-file app.env jfahrer/demo_web_app:latest
+
+docker container run -d --network mynet --network-alias webapp --name app2 --env-file app.env jfahrer/demo_web_app:latest
+
+docker container run -d --network mynet --network-alias webapp --name app3 --env-file app.env jfahrer/demo_web_app:latest
+
+docker container run -d --network mynet --network-alias webapp --name app4 --env-file app.env jfahrer/demo_web_app:latest
+
+docker container run -d --network mynet --network-alias webapp --name app5 --env-file app.env jfahrer/demo_web_app:latest
+
+docker container run -d --network mynet --name lb --env-file lb.env -p 80:80 ewajs/lb:latest
